aadf2717f0e05fae93a9e51cce7865b85d6f2803
Matthew Yacobucci <m.yacobucci@f5.com>
2018-07-10 14:52:28 -0600

    updating SDK files

--- f5/bigip/resource.py
+++ f5/bigip/resource.py
@@ -589,7 +589,6 @@ class ResourceBase(PathElement, ToDictMixin):
             try:
                 LOGGER.info("STARTING SESSION PUT")
                 LOGGER.info("UPDATE URI {}".format(update_uri))
-                LOGGER.info("DATA DICT {}".format(data_dict))
                 response = session.put(update_uri, json=data_dict, **requests_params)
                 self._meta_data = temp_meta
                 LOGGER.info("LOCAL UPDATE")
